d3377a0bac2a1636513ab4ec3585bdef82ed5606
michelecanini <studio.michelecanini@gmail.com>
2023-06-15 10:01:54 +0200

    [UPDATE] Insert fonts link and vue link

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// VUEJS APP
+const {createApp} = Vue;                                             
+
+createApp({
+        data(){                                
+            return{           
+                message: 'Hello vuejs!',
+            }     
+        }
+}).mount('#app');                 
