559da144ac67c9ac27b1c2a1cd670baf580c0f79
michelecanini <studio.michelecanini@gmail.com>
2023-06-18 15:52:51 +0200

    [UPLOAD] Add and remove tasks

--- js/script.js
+++ js/script.js
@@ -4,9 +4,57 @@
 const {createApp} = Vue;                                             
 
 createApp({
-        data(){                                
-            return{           
-                message: 'Hello vuejs!',
+        data(){
+            return{
+                new_task: '',
+                title: 'Vue Toboolis',
+                items: [
+                    {
+                        text: 'Fare la spesa',
+                        done: false
+                    },
+
+                    {
+                        text: 'Pulire la camera',
+                        done: true
+                    },
+
+                    {
+                        text: 'Fare la spesa',
+                        done: false
+                    },
+
+                    {
+                        text: 'Pulire la camera',
+                        done: true
+                    },
+                ]
             }     
+        },
+
+        methods:{
+
+            changeStatus(index){
+                //this.items[index].done = !this.items[index].done;
+                if ( this.items[index].done ) {
+                    this.items[index].done = false;
+                }
+                else {
+                    this.items[index].done = true;
+                }
+            },
+
+            deleteTask(index){
+                this.items.splice(index,1);
+            },
+
+            addTask(){
+                let obj = {
+                    text: this.new_task,
+                    done: false
+                }
+                this.items.push(obj);
+            }
         }
-}).mount('#app');                 
+
+}).mount('#app');
